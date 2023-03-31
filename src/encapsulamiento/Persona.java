55ae9c140568a62f4e770f8e0bace01b83e80a30
Kelvin Anibal <Anibalanibalvasquezromero@gmail.com>
2023-03-31 17:17:07 -0500

    Funcionalidad: Se ha creado la clase persona con campos de encapsulamiento

---
+++ src/encapsulamiento/Persona.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package encapsulamiento;
+
+/**
+ *
+ * @author chave
+ */
+public class Persona {
+    private String nombre;
+    private String dni;
+    private double peso;
+    
+    public Persona(String nombre, String dni, double peso) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+   
+    
+}
